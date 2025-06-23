97b2c20345efd5e515e2bcad2cd0981c705e833d
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-06-23 12:00:27 -0500

    Agregado de searchNode y toString

--- btree/BNode.java
+++ btree/BNode.java
@@ -3,16 +3,21 @@ package btree;
 import java.util.ArrayList;
 
 public class BNode<E extends Comparable<E>> {
+    // Contador estático para asignar IDs únicos a cada nodo
+    private static int counter = 0;
+    private int idNode;               // Identificador del nodo
     protected ArrayList<E> keys;
     protected ArrayList<BNode<E>> childs;
     protected int count;
 
     public BNode(int n) {
+        this.idNode = ++counter;
+
         this.keys   = new ArrayList<>(n);
         this.childs = new ArrayList<>(n);
-        this.count = 0;
+        this.count  = 0;
         for (int i = 0; i < n; i++) {
-            this.keys  .add(null);
+            this.keys .add(null);
             this.childs.add(null);
         }
     }
@@ -34,9 +39,14 @@ public class BNode<E extends Comparable<E>> {
         return (i < count && key.compareTo(keys.get(i)) == 0);
     }
 
+    public int getIdNode() {
+        return idNode;
+    }
+
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("[");
+        StringBuilder sb = new StringBuilder("ID");
+        sb.append(idNode).append(": [");
         for (int i = 0; i < count; i++) {
             sb.append(keys.get(i));
             if (i < count - 1) sb.append(", ");
@@ -45,3 +55,4 @@ public class BNode<E extends Comparable<E>> {
         return sb.toString();
     }
 }
+
